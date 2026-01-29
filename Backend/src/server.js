cda2b7df22824793aaa2b48255ac590a6e287e63
panu-27 <pranavzinjad06@gmail.com>
2026-01-29 21:30:17 +0530

    feat:added backend url

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,7 @@ const app = require("./app");
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT,"127.0.0.1", () => {
+    app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
